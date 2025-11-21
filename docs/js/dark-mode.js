20ecf4c89685840c122f4e06425f31f6b15aa796
Dennis Rudolph <dennisrudolph512@gmail.com>
2025-11-21 21:51:21 +0100

    added dark mode

--- docs/js/dark-mode.js
+++ docs/js/dark-mode.js
@@ -1,9 +1,13 @@
-<script>
-  // This script is designed for Bootstrap 5.3 +
-  window.addEventListener('DOMContentLoaded', (event) => {
+// This script is designed for Bootstrap 5.3 +
+window.addEventListener('DOMContentLoaded', (event) => {
 	const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 	if (prefersDark) {
-	  document.documentElement.setAttribute("data-bs-theme", "dark");
+    	document.documentElement.setAttribute("data-bs-theme", "dark");
+    	document.getElementById('darkModeSwitch').checked = true;
 	}
-  });
-</script>
+	document.getElementById('darkModeSwitch').addEventListener('change', function() {
+        let temp = document.getElementById('darkModeSwitch').checked;
+        document.documentElement.setAttribute("data-bs-theme", temp ? "dark" : "light");
+    });
+});
+
