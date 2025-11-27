0c9e12170afced50f5730f06cf20fd1e2e82dd1d
Dennis Rudolph <dennisrudolph512@gmail.com>
2025-11-27 11:31:47 +0100

    added preliminary data

---
+++ docs/js/data/actual-times.js
@@ -0,0 +1,6 @@
+/**
+ * Map that assigns the time strings of the actual times that were taken to beat a game to game numbers.
+ * @type {Object.<number, string>}
+ */
+const ACTUAL_TIMES = {
+};
---
+++ docs/js/data/games.js
@@ -0,0 +1,28 @@
+/**
+ * Map that assigns game titles to game numbers.
+ * @type {Object.<number, string>}
+ */
+const GAMES = {
+    330: "Tamagotchi Connection: Corner Shop 2",
+    331: "Pokemon Conquest",
+    332: "Battles of Prince of Persia",
+    333: "Call of Duty 4: Modern Warfare",
+    334: "Dragon Quest IX: Sentinels of the Starry Skies",
+    335: "Brain Age: Train Your Brain In Minutes A Day!",
+    336: "New Zealand Story Revolution",
+    337: "Tornado",
+    338: "Power Rangers: Super Legends",
+    339: "Space Camp",
+    340: "Suikoden: Tierkreis",
+    341: "Ant Nation",
+    342: "Family Feud",
+    343: "Imagine: Salon Stylist",
+    344: "Bleach: Dark Souls",
+    345: "Pokemon Heartgold",
+    346: "River City: Soccer Hooligans",
+    347: "Driver's Ed Portable: U.S.A. Edition",
+    348: "Petz: Bunnyz",
+    349: "Spelling Challenges and More!",
+    350: "Sudoku Gridmaster",
+    351: "Dokapon Journey"
+};
