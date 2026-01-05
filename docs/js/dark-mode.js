2e49d109963561a004d7ff9db14469c4912dfb7a
Dennis Rudolph <dennisrudolph512@gmail.com>
2026-01-05 21:26:31 +0100

    un-jquery'd dark mode

--- docs/js/dark-mode.js
+++ docs/js/dark-mode.js
@@ -1,14 +1,21 @@
 // This script is designed for Bootstrap 5.3 +
 let darkMode = false;
-$(document).ready(function () {
+function ready(fn) {
+	if (document.readyState !== 'loading') {
+		fn();
+	} else {
+		document.addEventListener('DOMContentLoaded', fn);
+	}
+};
+
+ready(function () {
 	const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 	if (prefersDark) {
     	document.documentElement.setAttribute("data-bs-theme", "dark");
 		darkMode = true;
 	}
-	$('#darkModeButton').on('click', function () {
+	document.getElementById('darkModeButton').addEventListener('click', function () {
 		darkMode = !darkMode;
 		document.documentElement.setAttribute("data-bs-theme", darkMode ? "dark" : "light");
 	});
 });
-
